38f5f901fbf2f99d6d890b440385095e532fcbc1
Susam Pal <susam@susam.in>
2018-12-24 18:41:30 +0530

    Run plugins in separate worker subprocesses
    This change enhances the `manager` module to invoke plugin methods in
    separate subprocesses. This allows all plugins to run in parallel.
    Records generated by one plugin is fed to another plugin via queues
    (`multiprocessing.Queue` objects).
    
    In the configuration file, there is an `audits` key which may contain
    multiple audit keys, e.g., `mockaudit`, `myaudit`, etc. Each such audit
    key defines a single audit definition. A single audit definition
    specifies cloud plugins, store plugins, check plugins, and alert
    plugins.
    
    All of these plugins together under a single audit key form a single
    audit definition. Multiple such audit definitions may be present under
    the "audits" key in the configuration.
    
    Here is how data flows between the various plugins within a single audit
    definition:
    
                           ,----> Store Plugins
        Cloud Plugins ----|
                           `----> Check Plugins ----> Alert Plugins
    
    Here is a detailed description of the above diagram:
    
      - At first, the cloud plugins retrieve (`read`) cloud configuration
        and metadata records from their respective clouds. All of these
        records from all cloud plugins are then fed to all store plugins and
        all store check plugins defined within the same audit definition.
    
      - Each store plugin would then typically store (`write`) these records
        to a persistent data store.
    
      - Each check plugin would evaluate (`eval`) each record to check for
        any interesting properties of the records. Typically, a check plugin
        would look for security misconfigurations in a record. For every
        such security misconfiguration or any other interesting property
        detected in a record, it may generate one or more event records.
        These event records are then fed to alert plugins.
    
      - Each alert plugin would then typically send or store (`write`) these
        records to a receiver or persistent data store, respectively.
    
    There are a few more important points to note:
    
      - The plugins in one audit definition and the plugins in another audit
        definition are completely independent of each other. The plugins in
        one audit definition cannot send/receive data to/from plugins in
        another audit definition. This allows each store or alert plugin to
        clear or reset their data store when an audit starts without
        losing the data obtained with an independent audit definition.
    
      - A store plugin is also a valid alert plugin because both of them
        must implement the same methods: `write` and `done`.
    
      - The plugins have no knowledge of each other. Each plugin only
        accepts and/or generates data that it is concerned with. They do not
        interact with another plugin. Thus, every arrow in the above ASCII
        diagram is a bit misleading. They are not direct connections from
        one plugin to another. There is always the `manager` module in
        between that takes data from one plugin and feeds it to another
        plugin.

---
+++ cloudmarker/workers.py
@@ -0,0 +1,66 @@
+"""Worker functions.
+
+The functions in this module wrap around plugin classes such that these
+worker functions can be specified as the ``target`` parameter while
+launching a new process with ``multiprocessing.Process``.
+
+Each worker function can run as a separate process. While wrapping
+around a plugin class, each worker function creates the appropriate
+multiprocessing queues to pass records from one plugin class to another.
+"""
+
+
+def cloud_worker(worker_name, cloud_plugin, output_queues):
+    """Cloud worker function.
+
+    Arguments:
+        worker_name (str): Display name for the worker.
+        cloud_plugin (object): Cloud plugin object.
+        output_queues (list): List of mp.Queue objects to write records to.
+    """
+    print('%s: Started' % worker_name)
+    for record in cloud_plugin.read():
+        for q in output_queues:
+            q.put(record)
+    print('%s: Stopped' % worker_name)
+
+
+def store_worker(worker_name, store_plugin, input_queue):
+    """Store worker function.
+
+    Arguments:
+        worker_name (str): Display name for the worker.
+        store_plugin (object): Store plugin object.
+        input_queue (multiprocessing.Queue): Queue to read records from.
+    """
+    print('%s: Started' % worker_name)
+    while True:
+        record = input_queue.get()
+        if record is None:
+            store_plugin.done()
+            break
+        store_plugin.write(record)
+    print('%s: Stopped' % worker_name)
+
+
+def check_worker(worker_name, check_plugin, input_queue, output_queues):
+    """Check worker function.
+
+    Arguments:
+        worker_name (str): Display name for the worker.
+        store_plugin (object): Store plugin object.
+        input_queue (multiprocessing.Queue): Queue to read records from.
+        output_queues (list): List of queues to write event records to.
+    """
+    print('%s: Started' % worker_name)
+    while True:
+        record = input_queue.get()
+        if record is None:
+            check_plugin.done()
+            break
+
+        for event_record in check_plugin.eval(record):
+            for q in output_queues:
+                q.put(event_record)
+
+    print('%s: Stopped' % worker_name)
