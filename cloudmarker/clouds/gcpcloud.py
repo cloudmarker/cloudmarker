bdd92eea1d86af2597e2eaff1420e1441ec7e1dc
Rohit Sehgal <rohitsehgal1994@gmail.com>
2019-01-05 17:48:58 +0530

    Add GCPCloud plugin

---
+++ cloudmarker/clouds/gcpcloud.py
@@ -0,0 +1,169 @@
+"""GCP cloud plugin.
+
+This module defines the GCPCloud class that contains the methods to
+pull data from Google cloud and return it to the calling manager.
+"""
+
+import json
+
+from google.oauth2 import service_account
+from googleapiclient import discovery
+
+# _GCP_SCOPES list defines the scopes which service account client can invoke
+# with the provided credentials. Currently GCPCloud module only fetches
+# Firewall and VM instances related information from the specified project, so
+# this list contains scope as compute.readonly. More information about scopes
+# can be located at
+# https://developers.google.com/identity/protocols/googlescopes
+_GCP_SCOPES = ['https://www.googleapis.com/auth/compute.readonly']
+
+
+class GCPCloud:
+    """GCP cloud plugin."""
+
+    def __init__(self, service_account_key_path, zone):
+        """Initialize the class with specified connection parameters.
+
+        Arguments:
+            service_account_key_path (str): Location of service account key
+              json file.
+            zone (str): The name of the zone for current project.
+        """
+        self._service_account_key_path = service_account_key_path
+        self._zone = zone
+
+        # Service account key file also have project name under the key
+        # project_id. We will use this key file to get the project name for
+        # this request.
+        with open(self._service_account_key_path) as f:
+            self._project_name = json.loads(f.read())['project_id']
+
+        # Generating scoped credentials which will be required by the
+        # discovery.build to create a resource object which we will use to
+        # communicate with the API.
+        credentials = service_account.Credentials
+        self._credentials = credentials.from_service_account_file(
+            self._service_account_key_path,
+            scopes=_GCP_SCOPES)
+
+        # Currently generating scoped resources with googleapiclient, requires
+        # service_name and version information, the more complete information
+        # about available_service name and their corresponding version is
+        # located at
+        # https://developers.google.com/api-client-library/python/apis/
+        self._compute_resource = self._get_resource('compute', 'v1')
+
+    def _get_resource(self, service_name, version='v1'):
+        """Construct and return Resource object for interacting with an API.
+
+        Arguments:
+            service_name (str): Name of the service of resource object.
+            version (str): Version of the API for resource object.
+
+        Returns:
+            Resource object for interacting with the API.
+
+        """
+        # cache_discovery is boolean keyword argument that tells builder
+        # whether or not to cache the discovery doc.
+        return discovery.build(service_name,
+                               version,
+                               credentials=self._credentials,
+                               cache_discovery=False)
+
+    def _get_firewall_rules(self):
+        """Query the compute resource and returns firewall rules.
+
+        Returns:
+            list: Firewall rules for specified project.
+
+        """
+        # Get firewall resource from the compute resource
+        firewall_resource = self._compute_resource.firewalls()
+
+        firewall_rules = []
+        next_page_token = None
+
+        while True:
+            # Prepare the request to get the list of all firewall rules for a
+            # project and execute that request.
+            firewall_rules_object = firewall_resource.list(
+                project=self._project_name,
+                pageToken=next_page_token).execute()
+
+            firewall_rules.extend(firewall_rules_object['items'])
+
+            # firewall_rules_object will only contain the maximum number of
+            # results per page which is specified in options keyword argument
+            # maxResults in list() API call. The default value of maxResults is
+            # 500, if the result contains more than 500 records, nextPageToken
+            # is present in response whose value is to be used to make the
+            # subsequent list() call.
+            if 'nextPageToken' not in firewall_rules_object.keys():
+                break
+
+            next_page_token = firewall_rules_object['nextPageToken']
+
+        return firewall_rules
+
+    def _get_instances(self):
+        """Query the compute resource and returns instance list.
+
+        Returns:
+            list: VM instances for specified project.
+
+        """
+        # Get instance resource to get the list of instances for a project and
+        # execute that request.
+        instance_resource = self._compute_resource.instances()
+        instances = []
+        next_page_token = None
+
+        while True:
+            # Prepare the request to get the list of all VM instances for a
+            # project and execute that request.
+            instances_object = instance_resource.list(
+                project=self._project_name,
+                pageToken=next_page_token,
+                zone=self._zone).execute()
+
+            instances.extend(instances_object['items'])
+
+            # instances_object will only contain the maximum number of results
+            # per page which is specified in options keyword argument
+            # maxResults in list() API call. The default value of maxResults is
+            # 500, if the result contains more than 500 records, nextPageToken
+            # key is present in response whose value is to be used to make the
+            # subsequent list() call.
+            if 'nextPageToken' not in instances_object.keys():
+                break
+
+            next_page_token = instances_object['nextPageToken']
+
+        return instances
+
+    def read(self):
+        """Return a record.
+
+        This method interacts with different resources and yields firewall
+        rules and instances for specified project.
+
+        Yields:
+            dict: Firewall rules and VM instances.
+
+        """
+        firewall_rules = self._get_firewall_rules()
+        instances = self._get_instances()
+
+        for rule in firewall_rules:
+            rule.update({'record_type': 'firewall_rule'})
+            print(rule)
+            yield rule
+
+        for instance in instances:
+            instance.update({'record_type': 'compute_instance'})
+            print(instance)
+            yield instance
+
+    def done(self):
+        """Perform clean up tasks."""
